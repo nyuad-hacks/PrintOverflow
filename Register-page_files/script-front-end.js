d6703aea7eb89ac39de8097e64b0c626bd478365
HorusXXVI <HorusXXVI@gmail.com>
2019-11-23 13:59:53 +0400

    add a dynamic profile page

--- Register-page_files/script-front-end.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-    jQuery('#wppb-register-user').submit(function (e) {
-        //stop submitting the form to see the disabled button effect
-        e.preventDefault();
-        //disable the submit button
-        jQuery('.form-submit #register').attr('disabled', true);
-        this.submit();
-    });
-});
